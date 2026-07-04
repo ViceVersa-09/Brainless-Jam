ecd265cd7eab7143b2bc522b21e7add5431a7e4a
ViceVersa-09 <viktor.versen@gmail.com>
2026-07-04 15:01:19 +0200

    base and summary
    built the base of the base and kept working on the summary

--- Assets/Scripts/Summary.cs
+++ Assets/Scripts/Summary.cs
@@ -1,11 +1,39 @@
+using TMPro;
 using UnityEngine;
 
 public class Summary : MonoBehaviour
 {
+    [Header("Bread")]
+    [SerializeField] TextMeshProUGUI breadText;
+    [SerializeField] TextMeshProUGUI timeText;
+
+    [Header("Wood")]
+    [SerializeField] TextMeshProUGUI missionWoodText;
+    [SerializeField] TextMeshProUGUI woodText;
+    [SerializeField] TextMeshProUGUI woodGainedText;
+
+    [Header("Wood")]
+    [SerializeField] TextMeshProUGUI missionStoneText;
+    [SerializeField] TextMeshProUGUI stoneText;
+    [SerializeField] TextMeshProUGUI stoneGainedText;
+
     ResourceManager resourceManager;
+    GameManager gameManager;
 
     private void Start()
     {
         resourceManager = FindFirstObjectByType<ResourceManager>();
+        gameManager = FindFirstObjectByType<GameManager>();
+    }
+
+    void Bread()
+    {
+        breadText.text = gameManager.bread + " Bread Baked";
+        timeText.text = "+" + (gameManager.bread * 15) + " seconds";
+    }
+
+    void Wood()
+    {
+
     }
 }
