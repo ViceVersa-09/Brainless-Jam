1d1c920b2f276e49ba4cf2937c35f35c4f1385db
SamuelHarris09 <samharris2009@hotmail.com>
2026-07-05 23:42:43 +0200

    TerrrainGenerator Script done

---
+++ Assets/Scripts/TerrainGenerator.cs
@@ -0,0 +1,62 @@
+using UnityEngine;
+
+public class TerrainGenerator : MonoBehaviour
+{
+    [Header("Tree Settings")]
+    [SerializeField] private int treeSpawnLimit = 20;
+    [SerializeField] private float treeDelay = 1f;
+    [SerializeField] private Vector2 miniTree = new(0f, 0f);
+    [SerializeField] private Vector2 maxiTree = new(0f, 0f);
+    [SerializeField] GameObject treePrefab;
+
+    [Header("Stone Settings")]
+    [SerializeField] private int stoneSpawnLimit = 20;
+    [SerializeField] private float stoneDelay = 1f;
+    [SerializeField] private Vector2 miniStone = new(0f, 0f);
+    [SerializeField] private Vector2 maxiStone = new(0f, 0f);
+    [SerializeField] GameObject stonePrefab;
+
+    [Header("Other stuff")]
+    [SerializeField] float startDelay = 10.0f;
+
+    int currentTreeSpawnCount;
+    int currentStoneSpawnCount;
+
+    void Start()
+    {
+        InvokeRepeating(nameof(SpawnTree), startDelay, treeDelay);
+        InvokeRepeating(nameof(SpawnStone), startDelay, stoneDelay);
+
+        currentTreeSpawnCount = 0;
+        currentStoneSpawnCount = 0;
+    }
+
+    private void SpawnTree()
+    {
+        if (currentTreeSpawnCount >= treeSpawnLimit)
+        {
+            CancelInvoke(nameof(SpawnTree));
+            return;
+        }
+
+        Vector2 spawnPosition = new(Random.Range(miniTree.x, maxiTree.x), Random.Range(miniTree.y, maxiTree.y));
+
+        Instantiate(treePrefab, spawnPosition, Quaternion.identity);
+        currentTreeSpawnCount++;
+
+    }
+
+    private void SpawnStone()
+    {
+        if (currentStoneSpawnCount >= stoneSpawnLimit)
+        {
+            CancelInvoke(nameof(SpawnStone));
+            return;
+        }
+
+        Vector2 spawnPosition = new(Random.Range(miniStone.x, maxiStone.x), Random.Range(miniStone.y, maxiStone.y));
+
+        Instantiate(stonePrefab, spawnPosition, Quaternion.identity);
+        currentStoneSpawnCount++;
+    }
+}
