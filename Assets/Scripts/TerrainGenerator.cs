48d6a823a8e13e954fa7268fd5c4c60575360a36
SamuelHarris09 <samharris2009@hotmail.com>
2026-07-06 12:03:12 +0200

    Updated TerrainGenerator
    Made its base values faster

--- Assets/Scripts/TerrainGenerator.cs
+++ Assets/Scripts/TerrainGenerator.cs
@@ -4,21 +4,21 @@ public class TerrainGenerator : MonoBehaviour
 {
     [Header("Tree Settings")]
     [SerializeField] private int treeSpawnLimit = 20;
-    [SerializeField] private float treeDelay = 1f;
+    [SerializeField] private float treeDelay = 0.1f;
     [SerializeField] private Vector2 miniTree = new(0f, 0f);
     [SerializeField] private Vector2 maxiTree = new(0f, 0f);
     [SerializeField] GameObject treePrefab;
 
     [Header("Stone Settings")]
     [SerializeField] private int stoneSpawnLimit = 20;
-    [SerializeField] private float stoneDelay = 1f;
+    [SerializeField] private float stoneDelay = 0.1f;
     [SerializeField] private Vector2 miniStone = new(0f, 0f);
     [SerializeField] private Vector2 maxiStone = new(0f, 0f);
     [SerializeField] GameObject stonePrefab;
 
     [Header("Other stuff")]
-    [SerializeField] private float startDelay = 10.0f;
-    [SerializeField] private float spawnCheckRadius = 0.5f;
+    [SerializeField] private float startDelay = 0.1f;
+    [SerializeField] private float spawnCheckRadius = 0.25f;
 
     private int currentTreeSpawnCount;
     private int currentStoneSpawnCount;
